c4b688efc3afe775edb0330b9e139c682c508336
Frederico Dellu <fredericobarreto@hotmail.com.br>
2025-09-06 16:33:59 -0300

    Acrescentado versão desktop, versão prototipo

---
+++ Appparalogin/Appparalogin/Cadastro_de_Usuarios.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Appparalogin
+{
+    partial class Cadastro_de_Usuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCadastroTel = new System.Windows.Forms.Label();
+            this.lblCadastroSenha = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCadastroCancel = new System.Windows.Forms.Button();
+            this.btnCadastroAdd = new System.Windows.Forms.Button();
+            this.txtCadastroTel = new System.Windows.Forms.TextBox();
+            this.txtCadastroSenha = new System.Windows.Forms.TextBox();
+            this.txtCadastroEmail = new System.Windows.Forms.TextBox();
+            this.txtCadastroNome = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(2, 58);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(59, 20);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email:";
+            // 
+            // lblCadastroTel
+            // 
+            this.lblCadastroTel.AutoSize = true;
+            this.lblCadastroTel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCadastroTel.Location = new System.Drawing.Point(2, 239);
+            this.lblCadastroTel.Name = "lblCadastroTel";
+            this.lblCadastroTel.Size = new System.Drawing.Size(79, 20);
+            this.lblCadastroTel.TabIndex = 2;
+            this.lblCadastroTel.Text = "Telefone: ";
+            // 
+            // lblCadastroSenha
+            // 
+            this.lblCadastroSenha.AutoSize = true;
+            this.lblCadastroSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCadastroSenha.Location = new System.Drawing.Point(2, 334);
+            this.lblCadastroSenha.Name = "lblCadastroSenha";
+            this.lblCadastroSenha.Size = new System.Drawing.Size(64, 20);
+            this.lblCadastroSenha.TabIndex = 3;
+            this.lblCadastroSenha.Text = "Senha: ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCadastroCancel);
+            this.groupBox1.Controls.Add(this.btnCadastroAdd);
+            this.groupBox1.Controls.Add(this.txtCadastroTel);
+            this.groupBox1.Controls.Add(this.txtCadastroSenha);
+            this.groupBox1.Controls.Add(this.txtCadastroEmail);
+            this.groupBox1.Controls.Add(this.txtCadastroNome);
+            this.groupBox1.Controls.Add(this.lblCadastroSenha);
+            this.groupBox1.Controls.Add(this.lblCadastroTel);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lblNome);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(31, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(819, 458);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cadastrar Novo Usuario";
+            // 
+            // btnCadastroCancel
+            // 
+            this.btnCadastroCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastroCancel.Location = new System.Drawing.Point(671, 396);
+            this.btnCadastroCancel.Name = "btnCadastroCancel";
+            this.btnCadastroCancel.Size = new System.Drawing.Size(130, 56);
+            this.btnCadastroCancel.TabIndex = 9;
+            this.btnCadastroCancel.Text = "Cancelar";
+            this.btnCadastroCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnCadastroAdd
+            // 
+            this.btnCadastroAdd.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCadastroAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastroAdd.Location = new System.Drawing.Point(522, 396);
+            this.btnCadastroAdd.Name = "btnCadastroAdd";
+            this.btnCadastroAdd.Size = new System.Drawing.Size(130, 56);
+            this.btnCadastroAdd.TabIndex = 8;
+            this.btnCadastroAdd.Text = "Adicionar novo usuario";
+            this.btnCadastroAdd.UseVisualStyleBackColor = false;
+            this.btnCadastroAdd.Click += new System.EventHandler(this.btnCadastroAdd_Click);
+            // 
+            // txtCadastroTel
+            // 
+            this.txtCadastroTel.Location = new System.Drawing.Point(6, 262);
+            this.txtCadastroTel.Name = "txtCadastroTel";
+            this.txtCadastroTel.Size = new System.Drawing.Size(387, 38);
+            this.txtCadastroTel.TabIndex = 7;
+            // 
+            // txtCadastroSenha
+            // 
+            this.txtCadastroSenha.Location = new System.Drawing.Point(6, 355);
+            this.txtCadastroSenha.Name = "txtCadastroSenha";
+            this.txtCadastroSenha.Size = new System.Drawing.Size(387, 38);
+            this.txtCadastroSenha.TabIndex = 6;
+            // 
+            // txtCadastroEmail
+            // 
+            this.txtCadastroEmail.Location = new System.Drawing.Point(6, 161);
+            this.txtCadastroEmail.Name = "txtCadastroEmail";
+            this.txtCadastroEmail.Size = new System.Drawing.Size(387, 38);
+            this.txtCadastroEmail.TabIndex = 5;
+            // 
+            // txtCadastroNome
+            // 
+            this.txtCadastroNome.Location = new System.Drawing.Point(6, 81);
+            this.txtCadastroNome.Name = "txtCadastroNome";
+            this.txtCadastroNome.Size = new System.Drawing.Size(387, 38);
+            this.txtCadastroNome.TabIndex = 4;
+            this.txtCadastroNome.TextChanged += new System.EventHandler(this.txtCadastroNome_TextChanged);
+            // 
+            // Cadastro_de_Usuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 504);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Cadastro_de_Usuarios";
+            this.Text = "Cadastro_de_Usuarios";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCadastroTel;
+        private System.Windows.Forms.Label lblCadastroSenha;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtCadastroTel;
+        private System.Windows.Forms.TextBox txtCadastroSenha;
+        private System.Windows.Forms.TextBox txtCadastroEmail;
+        private System.Windows.Forms.TextBox txtCadastroNome;
+        private System.Windows.Forms.Button btnCadastroAdd;
+        private System.Windows.Forms.Button btnCadastroCancel;
+    }
+}
---
+++ Appparalogin/Appparalogin/Login.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Appparalogin
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtUsuario.Text == "admin" && txtSenha.Text == "123")
+                {
+                    var menu = new MenuRestrito();
+
+                    menu.Show();
+
+
+                    this.Visible = false;
+                    //this.Dispose();
+                    
+
+                }
+                else
+                {
+                    MessageBox.Show("Usuário ou senha inválidos",
+                                    "Atenção!!",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+
+                    txtUsuario.Focus();
+                    txtSenha.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro: " + ex.Message,
+                                "Atenção!!",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+
+            }
+
+        }
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            txtUsuario.BackColor = Color.LightYellow;
+            if (txtUsuario.Text == "Digite seu usuário, apenas letras ou números.")
+            {
+                txtUsuario.Text = "";
+                txtUsuario.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtUsuario_Leave(object sender, EventArgs e)
+        {
+            txtUsuario.BackColor = Color.White;
+            if (txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "Digite seu usuário, apenas letras ou números.";
+                txtUsuario.ForeColor = SystemColors.InactiveCaption;
+            }
+        }
+
+        private void txtSenha_Enter(object sender, EventArgs e)
+        {
+            txtSenha.BackColor = Color.LightYellow;
+            if (txtSenha.Text == "Senha")
+            {
+                txtSenha.Text = "";
+                txtSenha.ForeColor = Color.Black;
+                txtSenha.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtSenha_Leave(object sender, EventArgs e)
+        {
+            txtSenha.BackColor = Color.White;
+            if (txtSenha.Text == "")
+            {
+                txtSenha.Text = "Senha";
+                txtSenha.ForeColor = SystemColors.InactiveCaption;
+                txtSenha.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void txtUsuario_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int tecla = (int)e.KeyChar;
+            
+            if (!char.IsLetterOrDigit(e.KeyChar) && tecla != 08)
+            {
+                e.Handled = true;
+                MessageBox.Show("Digite apenas letras ou números",
+                                    "Ops",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Warning);
+
+                txtUsuario.SelectionStart = 0;
+                txtUsuario.SelectionLength = txtUsuario.Text.Length;
+
+                txtUsuario.Focus();
+            }
+        }
+    }
+}
