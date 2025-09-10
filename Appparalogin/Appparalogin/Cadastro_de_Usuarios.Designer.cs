31488e60a075e60935eeb4a4594597937ad79605
Gabriel Chiaramonte <gchiaramonte2004@gmail.com>
2025-09-10 15:51:00 -0300

    Arumando o forms

--- Appparalogin/Appparalogin/Cadastro_de_Usuarios.Designer.cs
+++ Appparalogin/Appparalogin/Cadastro_de_Usuarios.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cadastro_de_Usuarios));
             this.lblNome = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.lblCadastroTel = new System.Windows.Forms.Label();
@@ -39,51 +40,68 @@
             this.txtCadastroSenha = new System.Windows.Forms.TextBox();
             this.txtCadastroEmail = new System.Windows.Forms.TextBox();
             this.txtCadastroNome = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblNome
             // 
             this.lblNome.AutoSize = true;
-            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNome.Location = new System.Drawing.Point(2, 58);
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(8, 96);
+            this.lblNome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNome.Name = "lblNome";
-            this.lblNome.Size = new System.Drawing.Size(59, 20);
+            this.lblNome.Size = new System.Drawing.Size(109, 36);
             this.lblNome.TabIndex = 0;
             this.lblNome.Text = "Nome: ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(2, 138);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 201);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.Size = new System.Drawing.Size(96, 36);
             this.label2.TabIndex = 1;
             this.label2.Text = "Email:";
             // 
             // lblCadastroTel
             // 
             this.lblCadastroTel.AutoSize = true;
-            this.lblCadastroTel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCadastroTel.Location = new System.Drawing.Point(2, 239);
+            this.lblCadastroTel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCadastroTel.Location = new System.Drawing.Point(8, 302);
+            this.lblCadastroTel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCadastroTel.Name = "lblCadastroTel";
-            this.lblCadastroTel.Size = new System.Drawing.Size(79, 20);
+            this.lblCadastroTel.Size = new System.Drawing.Size(146, 36);
             this.lblCadastroTel.TabIndex = 2;
             this.lblCadastroTel.Text = "Telefone: ";
+            this.lblCadastroTel.Click += new System.EventHandler(this.lblCadastroTel_Click);
             // 
             // lblCadastroSenha
             // 
             this.lblCadastroSenha.AutoSize = true;
-            this.lblCadastroSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCadastroSenha.Location = new System.Drawing.Point(2, 334);
+            this.lblCadastroSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCadastroSenha.Location = new System.Drawing.Point(8, 393);
+            this.lblCadastroSenha.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCadastroSenha.Name = "lblCadastroSenha";
-            this.lblCadastroSenha.Size = new System.Drawing.Size(64, 20);
+            this.lblCadastroSenha.Size = new System.Drawing.Size(117, 36);
             this.lblCadastroSenha.TabIndex = 3;
             this.lblCadastroSenha.Text = "Senha: ";
+            this.lblCadastroSenha.Click += new System.EventHandler(this.lblCadastroSenha_Click);
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightGray;
             this.groupBox1.Controls.Add(this.btnCadastroCancel);
             this.groupBox1.Controls.Add(this.btnCadastroAdd);
             this.groupBox1.Controls.Add(this.txtCadastroTel);
@@ -95,19 +113,23 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.lblNome);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(31, 31);
+            this.groupBox1.Location = new System.Drawing.Point(268, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(819, 458);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(982, 715);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Cadastrar Novo Usuario";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // btnCadastroCancel
             // 
             this.btnCadastroCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCadastroCancel.Location = new System.Drawing.Point(671, 396);
+            this.btnCadastroCancel.Location = new System.Drawing.Point(760, 619);
+            this.btnCadastroCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCadastroCancel.Name = "btnCadastroCancel";
-            this.btnCadastroCancel.Size = new System.Drawing.Size(130, 56);
+            this.btnCadastroCancel.Size = new System.Drawing.Size(173, 69);
             this.btnCadastroCancel.TabIndex = 9;
             this.btnCadastroCancel.Text = "Cancelar";
             this.btnCadastroCancel.UseVisualStyleBackColor = true;
@@ -116,9 +138,10 @@
             // 
             this.btnCadastroAdd.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btnCadastroAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCadastroAdd.Location = new System.Drawing.Point(522, 396);
+            this.btnCadastroAdd.Location = new System.Drawing.Point(517, 619);
+            this.btnCadastroAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCadastroAdd.Name = "btnCadastroAdd";
-            this.btnCadastroAdd.Size = new System.Drawing.Size(130, 56);
+            this.btnCadastroAdd.Size = new System.Drawing.Size(173, 69);
             this.btnCadastroAdd.TabIndex = 8;
             this.btnCadastroAdd.Text = "Adicionar novo usuario";
             this.btnCadastroAdd.UseVisualStyleBackColor = false;
@@ -126,42 +149,120 @@
             // 
             // txtCadastroTel
             // 
-            this.txtCadastroTel.Location = new System.Drawing.Point(6, 262);
+            this.txtCadastroTel.Location = new System.Drawing.Point(197, 296);
+            this.txtCadastroTel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCadastroTel.Name = "txtCadastroTel";
-            this.txtCadastroTel.Size = new System.Drawing.Size(387, 38);
+            this.txtCadastroTel.Size = new System.Drawing.Size(515, 46);
             this.txtCadastroTel.TabIndex = 7;
             // 
             // txtCadastroSenha
             // 
-            this.txtCadastroSenha.Location = new System.Drawing.Point(6, 355);
+            this.txtCadastroSenha.Location = new System.Drawing.Point(197, 393);
+            this.txtCadastroSenha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCadastroSenha.Name = "txtCadastroSenha";
-            this.txtCadastroSenha.Size = new System.Drawing.Size(387, 38);
+            this.txtCadastroSenha.Size = new System.Drawing.Size(515, 46);
             this.txtCadastroSenha.TabIndex = 6;
             // 
             // txtCadastroEmail
             // 
-            this.txtCadastroEmail.Location = new System.Drawing.Point(6, 161);
+            this.txtCadastroEmail.Location = new System.Drawing.Point(197, 195);
+            this.txtCadastroEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCadastroEmail.Name = "txtCadastroEmail";
-            this.txtCadastroEmail.Size = new System.Drawing.Size(387, 38);
+            this.txtCadastroEmail.Size = new System.Drawing.Size(515, 46);
             this.txtCadastroEmail.TabIndex = 5;
             // 
             // txtCadastroNome
             // 
-            this.txtCadastroNome.Location = new System.Drawing.Point(6, 81);
+            this.txtCadastroNome.Location = new System.Drawing.Point(197, 96);
+            this.txtCadastroNome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCadastroNome.Name = "txtCadastroNome";
-            this.txtCadastroNome.Size = new System.Drawing.Size(387, 38);
+            this.txtCadastroNome.Size = new System.Drawing.Size(515, 46);
             this.txtCadastroNome.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGray;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(230, 754);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 246);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Meus Chamados";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(48, 309);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "FAQ";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Appparalogin.Properties.Resources.unknown_5695541;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 309);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(42, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Appparalogin.Properties.Resources.menu_4204600;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 246);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(42, 25);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-34, -57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(295, 242);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
             // 
             // Cadastro_de_Usuarios
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(869, 504);
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(1277, 754);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Cadastro_de_Usuarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastro_de_Usuarios";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -179,5 +280,11 @@
         private System.Windows.Forms.TextBox txtCadastroNome;
         private System.Windows.Forms.Button btnCadastroAdd;
         private System.Windows.Forms.Button btnCadastroCancel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label3;
     }
 }
