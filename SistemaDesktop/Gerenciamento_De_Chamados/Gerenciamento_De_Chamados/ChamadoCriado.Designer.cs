d423bfe5c84207174011e5bee8c7a48d38406a42
Frederico Dellu <fredericobarreto@hotmail.com.br>
2025-10-06 01:34:48 -0300

    Colocando dados do formulario

--- SistemaDesktop/Gerenciamento_De_Chamados/Gerenciamento_De_Chamados/ChamadoCriado.Designer.cs
+++ SistemaDesktop/Gerenciamento_De_Chamados/Gerenciamento_De_Chamados/ChamadoCriado.Designer.cs
@@ -39,6 +39,25 @@
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.gbxChamadoCriado = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDataChamadoCriado = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
@@ -46,6 +65,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.gbxChamadoCriado.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel2
@@ -107,7 +128,7 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(172, 672);
+            this.panel1.Size = new System.Drawing.Size(172, 689);
             this.panel1.TabIndex = 32;
             // 
             // pictureBox2
@@ -168,19 +189,223 @@
             // gbxChamadoCriado
             // 
             this.gbxChamadoCriado.BackColor = System.Drawing.SystemColors.InactiveBorder;
-            this.gbxChamadoCriado.Location = new System.Drawing.Point(205, 95);
+            this.gbxChamadoCriado.Controls.Add(this.label19);
+            this.gbxChamadoCriado.Controls.Add(this.label18);
+            this.gbxChamadoCriado.Controls.Add(this.label17);
+            this.gbxChamadoCriado.Controls.Add(this.label16);
+            this.gbxChamadoCriado.Controls.Add(this.label15);
+            this.gbxChamadoCriado.Controls.Add(this.label14);
+            this.gbxChamadoCriado.Controls.Add(this.label13);
+            this.gbxChamadoCriado.Controls.Add(this.label12);
+            this.gbxChamadoCriado.Controls.Add(this.label11);
+            this.gbxChamadoCriado.Controls.Add(this.label10);
+            this.gbxChamadoCriado.Controls.Add(this.label8);
+            this.gbxChamadoCriado.Controls.Add(this.groupBox1);
+            this.gbxChamadoCriado.Location = new System.Drawing.Point(205, 85);
             this.gbxChamadoCriado.Name = "gbxChamadoCriado";
-            this.gbxChamadoCriado.Size = new System.Drawing.Size(660, 576);
+            this.gbxChamadoCriado.Size = new System.Drawing.Size(660, 586);
             this.gbxChamadoCriado.TabIndex = 34;
             this.gbxChamadoCriado.TabStop = false;
-            this.gbxChamadoCriado.Text = "groupBox1";
             this.gbxChamadoCriado.Paint += new System.Windows.Forms.PaintEventHandler(this.gbxChamadoCriado_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Chamado criado em:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(295, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "por ";
+            // 
+            // lblDataChamadoCriado
+            // 
+            this.lblDataChamadoCriado.AutoSize = true;
+            this.lblDataChamadoCriado.Location = new System.Drawing.Point(193, 15);
+            this.lblDataChamadoCriado.Name = "lblDataChamadoCriado";
+            this.lblDataChamadoCriado.Size = new System.Drawing.Size(25, 13);
+            this.lblDataChamadoCriado.TabIndex = 2;
+            this.lblDataChamadoCriado.Text = "opa";
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(360, 17);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(35, 13);
+            this.lblUser.TabIndex = 3;
+            this.lblUser.Text = "label5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(11, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(74, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = ">";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(99, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lblUser);
+            this.groupBox1.Controls.Add(this.lblDataChamadoCriado);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(0, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(659, 128);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(22, 161);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(231, 25);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Dados do Formulário";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(23, 210);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(249, 24);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Informações do Chamado";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(23, 246);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(283, 16);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "1. Problema está impedindo o trabalho?";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(24, 273);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(221, 16);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "2. Quais as pessoas afetadas?";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(24, 300);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(280, 16);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "3. Selecione o nome do Colaborador(a)";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(24, 326);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(93, 16);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "4. Telefone: ";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(23, 353);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(286, 16);
+            this.label15.TabIndex = 14;
+            this.label15.Text = "5. Selecione a solicitação do problema: ";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(24, 382);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(160, 16);
+            this.label16.TabIndex = 15;
+            this.label16.Text = "6. Numero do pedido: ";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(24, 412);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(291, 16);
+            this.label17.TabIndex = 16;
+            this.label17.Text = "7. Informe a data e horario da transação: ";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.Location = new System.Drawing.Point(24, 438);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(207, 16);
+            this.label18.TabIndex = 17;
+            this.label18.Text = "8. Descreva sua solicitação: ";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label19.Location = new System.Drawing.Point(23, 535);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(173, 16);
+            this.label19.TabIndex = 18;
+            this.label19.Text = "9. Documento anexado: ";
             // 
             // ChamadoCriado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1119, 672);
+            this.ClientSize = new System.Drawing.Size(1119, 689);
             this.Controls.Add(this.gbxChamadoCriado);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
@@ -195,6 +420,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.gbxChamadoCriado.ResumeLayout(false);
+            this.gbxChamadoCriado.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -212,5 +441,24 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.GroupBox gbxChamadoCriado;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblDataChamadoCriado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
     }
 }
