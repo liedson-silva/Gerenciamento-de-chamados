bb422aa91f3333642f86ab8abea174f5b2c14937
liedson-silva <liedsonsilva987@gmail.com>
2025-12-01 13:44:41 -0300

    feat: Web conectado ao beck hospedado no vercel

--- SistemaWeb_E_Mobile/frontEndWeb/src/services/api.js
+++ SistemaWeb_E_Mobile/frontEndWeb/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://gerenciamento-de-chamados.vercel.app"
 })
 
 // Adiciona o token no header `authorization` em todas as requisições
